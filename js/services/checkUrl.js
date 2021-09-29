59b972d148aa250ea0941f447253ee901ced7a7b
Anton <anton27081986@gmail.com>
2021-09-29 06:39:30 +0300

    authorization modification

--- js/services/checkUrl.js
+++ js/services/checkUrl.js
@@ -1,13 +1,27 @@
 import {LocalStorageService} from './localStorage.js';
 import {configObj} from '../environments/environments_production.js';
 
+const exitLink = document.querySelector('.exit__reg__link');
+
 function checkUrl() {
-    if (window.location.href.indexOf('auth.html') != -1 && LocalStorageService.getItemStorage('idToken')) {
-        document.location.replace(configObj.urlAdmin);
+    if (LocalStorageService.getItemStorage('idToken')) {
+        exitLink.href = configObj.urlAdmin;
     }
-    if (window.location.href.indexOf('admin.html') != -1 && !LocalStorageService.getItemStorage('idToken')) {
-        document.location.replace(configObj.urlAuth);
+    if (!LocalStorageService.getItemStorage('idToken')) {
+        exitLink.href = configObj.urlAuth;
     }
 }
-
+    
 checkUrl();
+
+
+// function checkUrl() {
+//     if (window.location.href.indexOf('auth.html') != -1 && LocalStorageService.getItemStorage('idToken')) {
+//         document.location.replace(configObj.urlAdmin);
+//     }
+//     if (window.location.href.indexOf('admin.html') != -1 && !LocalStorageService.getItemStorage('idToken')) {
+//         document.location.replace(configObj.urlAuth);
+//     }
+// }
+
+// checkUrl();
