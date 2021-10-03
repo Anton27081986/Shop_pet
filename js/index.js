e093f5db2b2234a7368c0e47a4928c5288727464
Anton <anton27081986@gmail.com>
2021-10-03 07:06:10 +0300

    modified add to cart

--- js/index.js
+++ js/index.js
@@ -2,7 +2,3 @@ import {getAllCard} from './components/getAllCard.js';
 import {addProdToCart} from './components/buyWatch.js';
 
 getAllCard();
-
-setTimeout(() => {
-    addProdToCart();
-}, 1000);
--- js/services/APIService.js
+++ js/services/APIService.js
@@ -1,15 +1,17 @@
 import { postData } from './postData.js';
 import {getData} from './getData.js';
+import {addProdToCart} from '../components/buyWatch.js';
 
 
 export class ApiInteraction {
     static createNewCard(data) {
         postData('https://shop-watch-81f60-default-rtdb.firebaseio.com/card.json', data);
     }
+
     static getCard() {
         getData('https://shop-watch-81f60-default-rtdb.firebaseio.com/card.json')
         .then(data => Object.values(data))
-        .then(data => {
+        .then(data => new Promise ((resolve, reject) => {
             data.forEach(item => {
                 document.querySelector('.new__arrivals__cards').insertAdjacentHTML('afterbegin', `
                     <div class="card" style="display: flex">
@@ -22,8 +24,37 @@ export class ApiInteraction {
                     </div>
                 `);
             });
-        });
+            resolve();
+        })
+        .then(addProdToCart()));
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+// static getCard() {
+    //     getData('https://shop-watch-81f60-default-rtdb.firebaseio.com/card.json')
+    //     .then(data => Object.values(data))
+    //     .then(data => {
+    //         data.forEach(item => {
+    //             document.querySelector('.new__arrivals__cards').insertAdjacentHTML('afterbegin', `
+    //                 <div class="card" style="display: flex">
+    //                     <div style="margin-left: 100px; margin-top: 40px">
+    //                         <img style="background-color: white; width: 220px; height: 320px;" class="img" src="${item.url}" alt="">
+    //                         <p class="name__card" style="color: white; font-family: 'Playfair Display'; font-size: 18px">${item.name}</p>
+    //                         <p class="price__card" style="color: white; font-family: 'Playfair Display'; font-size: 18px">${item.price} ${item.currency}</p>
+    //                         <button class="btn__buy">Купить</button>
+    //                     </div>
+    //                 </div>
+    //             `);
+    //         });
+    //     });
+    // }
